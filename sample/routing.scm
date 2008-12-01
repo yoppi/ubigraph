0416871a00369c9c6fdcbf1b331f6c07269131a1
yoppi <yoshida@cslimac.local>
2008-12-01 16:02:05 +0900

    add: routing.scm as a sample code

--- sample/routing.scm
+++ sample/routing.scm
@@ -1,11 +1,11 @@
 (add-load-path "../lib")
 (use srfi-1)
 (use srfi-13)
+(use srfi-27)
 (use ubigraph)
 
 (define *server* (init-ubigraph))
-(define *dim* 3) ; Number of dimensions for hypercube
-;(define *active-vertex* (make-vertex *server*))
+(define *dim* 7) ; Number of dimensions for hypercube
 (define *r* (expt 2 *dim*))
 
 (clear *server*)
@@ -23,11 +23,20 @@
   (set-style-vertex "color" "#F4FF85" *server*)
   (set-style-vertex "size" "0.25" *server*))
 
+(define (set-active-vertex v)
+  (set-color-vertex v "#FF4B30" *server*))
+
+(define (set-init-vertex v)
+  (set-shape-vertex v "sphere" *server*)
+  (set-color-vertex v "#F4FF85" *server*)
+  (set-size-vertex v "0.25" *server*))
+
 (define (integer->binary n)
   (define (loop i ret)
     (cond [(<= i 0)
            (if (string-null? ret) "0" ret)]
-          [else (loop (quotient i 2) (string-join (list (number->string (remainder i 2)) ret) ""))]))
+          [else (loop (quotient i 2) 
+                      (string-join (list (number->string (remainder i 2)) ret) ""))]))
   (loop n ""))
 
 (define (power))
@@ -45,16 +54,52 @@
                          (e (+ (* i *r*) (+ i k))))
                     (when (eq? (logand i k) 0)
                       (make-edge i (+ i k) *server* e)
-                      (set-strength-edge e (number->string (expt (/ 1.0 (+ j 1)) 1.5)) *server*))))
+                      (set-strength-edge e 
+                                         (number->string (expt (/ 1.0 (+ j 1)) 1.5)) *server*))))
                 (iota *dim* 0)))
               (iota *r* 0)))
   (loop1 0)
   (loop2))
 
+(define (animate-arrow e rev)
+  (let ((pos (cond [(string=? rev "true") 1.0] 
+                   [(string=? rev "false") 0.0]
+                   [else (error "animate-arrow needs 
+                                \"true\" or \"false\", but got;" rev)])))
+    (set-arrowposition-edge e (number->string pos) *server*)
+    (set-arrowreverse-edge e rev *server*)
+    (set-arrow-edge e "true" *server*)
+    (for-each (lambda (i)
+                (let ((a (if (string=? rev "true") (- 1.0 (/ i 19.0)) (/ i 19.0))))
+                  (set-arrowposition-edge e (number->string a) *server*)
+                  (sys-nanosleep 50000000))) (iota 20 0))
+    (set-arrow-edge e "false" *server*)))
+
+
 ;;========== Main
 (define (main args)
   (invisible-graph)
   (init-vertex-style)
-  ;(set-color-vertex *active-vertex* "#FF4B30" *server* )
   (init-graph)
-  (reveal-graph))
+  (reveal-graph)
+  (let ((pos 0))
+    (for-each (lambda (i) 
+      (let ((next-pos (random-integer *r*))
+            (v-from pos))
+        (set-active-vertex pos)
+        (set-active-vertex next-pos)
+        (for-each (lambda (j)
+                    (let* ((k (expt 2 j))
+                           (diff (- (logand next-pos k) (logand pos k)))
+                           (v-to (+ v-from diff))
+                           (e (if (<= v-from v-to) 
+                                (+ (* v-from *r*) v-to)
+                                (+ (* v-to *r*) v-from))))
+                      (cond [(> diff 0) (animate-arrow e "false")]
+                            [(< diff 0) (animate-arrow e "true")])
+                      (set! v-from v-to)))
+                  (iota *dim* 0))
+        (set-init-vertex pos)
+        (sys-nanosleep 500000000)
+        (set! pos next-pos))) 
+              (iota 10 0))))
