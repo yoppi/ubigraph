dda7231a2ec6b6283e6b29703bac272d6ea4af70
yoppi <yoshida@cslimac.local>
2008-12-06 17:46:04 +0900

    modified

--- sample/coroutine.scm
+++ sample/coroutine.scm
@@ -3,10 +3,9 @@
 (use ubigraph)
 
 (define *server* (init-ubigraph))
+(define *tasks* (make-queue))
 
 (clear *server*)
-
-(define *tasks* (make-queue))
 
 (define-syntax define-coroutine
   (syntax-rules ()
@@ -19,7 +18,7 @@
                                (return))))
                   body ...))
        ((dequeue! *tasks*)))]
-    [(_ (routine yield) body ...)
+    [(_ (routine (yield exit)) body ...)
      (define (routine)
        (call/cc (lambda (escape)
                   (call/cc (lambda (return)
@@ -32,36 +31,50 @@
                                           (enqueue! *tasks* cont)
                                           (escape))))
                              body ...))
-                  ((dequeue! *tasks*)))))]))
+                  ((dequeue! *tasks*)))))]
+    [(_ (routine n (yield)) body ...)
+     (define (routine n)
+       (call/cc (lambda (return)
+                  (define (yield)
+                    (call/cc (lambda (cont)
+                               (enqueue! *tasks* cont)
+                               (return))))
+                  body ...))
+       ((dequeue! *tasks*)))]
+    ))
 
 (define (coroutine-init! . rs)
   (set! *tasks* (make-queue))
-  (for-each (lambda (r)
+  (for-each (lambda (r) 
               (enqueue! *tasks* r)) rs))
 
 
 (define *val* #f)
 
-(define-coroutine (printer yield)
+(define-coroutine (printer (yield exit))
   (let ((parent #f))
     (define (loop)
-      (make-verex *val* *server*)
-      (when (not parent)
-        (make-edge parent *val* *server*))
-      (set! parent *val*)
-      (yield))
+      (cond [(equal? parent *val*) (exit)]
+            [else 
+              (make-vertex  *server* *val*)
+              (when (not (equal? parent #f))
+                (make-edge parent *val* *server*))
+              (set! parent *val*)
+              (yield)
+              (loop)]))
     (loop)))
 
-(define-coroutine (evaluator yield)
+(define-coroutine (evaluator n (yield))
   (define (loop)
     (for-each (lambda (i)
                 (set! *val* i)
-                (sys-sleep 1)
-                (yield)) (iota 100 0)))
+                (sys-nanosleep 20000000)
+                (yield)) 
+              (iota n 1)))
   (loop))
 
 
 ;;========== Main
 (define (main args)
   (coroutine-init! printer)
-  (evaluator))
+  (evaluator 10))
