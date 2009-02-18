d9691e51524d61e7027c98d6bf4950509b129266
yoppi <yoshida@macbook.local>
2009-02-18 10:22:25 +0900

    modified: changed a client obj scope from local to global.

--- sample/sample2.scm
+++ sample/sample2.scm
@@ -1,14 +1,15 @@
 (add-load-path "../lib")
 (use ubigraph)
 
+(define *server* (init-ubigraph))
+(clear *server*) ; clear the canvas.
+
 ;;========== Main
 (define (main args)
-  (let ((server (init-ubigraph)))
-    (let* ((v1 (make-vertex server))
-          (v2 (make-vertex server))
-          (e12 (make-edge v1 v2 server)))
-      (for-each (lambda (x) (set-size-vertex x "3.0" server)) (list v1 v2))
-      (set-shapedetail-vertex v1 "30" server)
-      (set-shape-vertex v1 "torus" server)
-      (set-)))
-  )
+  (let* ((v1 (make-vertex *server*)
+         (v2 (make-vertex *server*))
+         (e12 (make-edge v1 v2 *server*))))
+    (for-each (lambda (x) (set-size-vertex x "3.0" *server*)) (list v1 v2))
+    (set-shapedetail-vertex v1 "30" *server*)
+    (set-shape-vertex v1 "torus" *server*)))
+
